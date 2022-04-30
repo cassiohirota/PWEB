51205880621c7026960fcda036b5f6a3c69dab00
cassiohirota <41903845+cassiohirota@users.noreply.github.com>
2022-04-30 10:00:30 -0300

    Update atividade7.js

--- Atividade 7/atividade7.js
+++ Atividade 7/atividade7.js
@@ -1,8 +1,9 @@
 var tipo;
+var escolha1 = prompt("Escolha as opções\n 1 - Pedra \n 2 - Papel \n 3 - Tesoura");
+var escolha2 = Math.random();
 
-    var escolha1 = prompt("Escolha as opções\n 1 - Pedra \n 2 - Papel \n 3 - Tesoura");
-    if(escolha1 < 1 && escolha1 > 3)
-        alert("Digite valores entre 1 a 3!");
+if(escolha1 < 1 && escolha1 > 3)
+    alert("Digite valores entre 1 a 3!");
 
 if (escolha1 == 1)
 tipo = "Pedra";
@@ -10,8 +11,6 @@ if (escolha1 == 2)
 tipo = "Papel";
 if(escolha1 == 3)
 tipo = "Tesoura";
-
-var escolha2 = Math.random();
 
 if (escolha2 <= 0.33)
     escolha2 = "Pedra";
